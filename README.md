2b28afe34d7c22e9f3420e7fc0c8d4b8b3550e9d
daniel-mahmoodi <mahmoodidanial2@gmail.com>
2021-07-04 09:24:58 +0430

    go to app.js

---
+++ README.md
@@ -0,0 +1,5 @@
+https://ip-api.com/docs/api:json
+
+https://openweathermap.org/current
+
+https://home.openweathermap.org/users/sign_in
---
+++ app.js
@@ -0,0 +1,2 @@
+const url1 = 'http://ip-api.com/json/?fields=country,city,lat,lon,timezone';
+const url2 = 'api.openweathermap.org/data/2.5/weather?lat={1.35208}&lon={103.82}&appid={df1c2fec2a1282363c581e3d9777b14d}'
